f2949649ca86fdf9c602003d1f8f09bca54860ea
sin <godcharlie1018@gmail.com>
2020-01-15 22:00:29 +0800

    Win all algorithm

--- source/algorithm_A2.cpp
+++ source/algorithm_A2.cpp
@@ -100,7 +100,6 @@ void algorithm_A2(Board board, Player player, int index[]){
     int gmaeoverNumbers = 0;
     WinPoint Enemy[height][width];
     playerinfo P[height][width];
-    int boardcost[height][width];
     int boardconnect[height][width];
 
     Player *enemy; //敵人
@@ -113,27 +112,6 @@ void algorithm_A2(Board board, Player player, int index[]){
         color = BLUE;
         enemy = new Player(RED);
         enemy_color = RED;
-    }
-
-    for(int i = 0;i<height;i++){
-        for(int j = 0;j<width;j++){
-            if(i == 0&& j == 0){
-                boardcost[i][j] = 2;
-            }
-            else if(i == 0 && (j>0 || j<width-1)){
-                boardcost[i][j] = 3;
-            }
-            else if( i == height-1 && (j>0 || j<width-1)){
-                boardcost[i][j] = 3;
-            }
-            else if( j == 0 && (i>0 && i< height-1)){
-                boardcost[i][j] = 3;
-            }
-            else if(j == width && (i>0 && i< height-1)){
-                boardcost[i][j] = 3;
-            }
-            else boardcost[i][j] = 4;
-        }
     }
 
     for(int i = 0 ;i<height;i++){
@@ -198,19 +176,19 @@ void algorithm_A2(Board board, Player player, int index[]){
                 return;
             }
             else if(board.get_orbs_num(0,5) == 0){
-                cout<<"BB";
+                //cout<<"BB";
                 index[0] = 0;
                 index[1] = 5;
                 return;
             }
             else if(board.get_orbs_num(4,0) == 0){
-                cout<<"CC";
+                //cout<<"CC";
                 index[0] = 4;
                 index[1] = 0;
                 return ;
             }
             else if(board.get_orbs_num(4,5) == 0){
-                cout<<"dd";
+                //cout<<"dd";
                 index[0] = 4;
                 index[1] = 5;
                 return;
@@ -228,8 +206,7 @@ void algorithm_A2(Board board, Player player, int index[]){
                     for(int k = 0;k<height;k++){
                         for(int h = 0;h<width;h++){
                             if(tmp.get_cell_color(k,h) == color){
-                                
-                                P[i][j].score++;
+                                P[i][j].eatenemy++;
                                 /*if(board.get_orbs_num(k,h) == board.get_capacity(k,h)-1){
                                     P[i][j].score++;
                                 }*/
@@ -272,7 +249,27 @@ void algorithm_A2(Board board, Player player, int index[]){
                         }
                     }
                     
-                    
+                    //corn
+                    if(i == 0 && j== 0){
+                        if(board.get_orbs_num(i,j) == 0){
+                            P[i][j].score+=2;
+                        }
+                    }
+                    else if(i == 0 && j == width-1){
+                        if(board.get_orbs_num(i,j) == 0){
+                            P[i][j].score+=2;
+                        }
+                    }
+                    else if(i == height-1 && j == 0){
+                        if(board.get_orbs_num(i,j) == 0){
+                            P[i][j].score+=2;
+                        }
+                    }
+                    else if(i == height && j == width-1){
+                        if(board.get_orbs_num(i,j) == 0){
+                            P[i][j].score+=2;
+                        }
+                    }
                     
                     //side
                     if(i == 0 && (j<width-1 || j>0)){
@@ -333,7 +330,7 @@ void algorithm_A2(Board board, Player player, int index[]){
                     if(!Enemy[i][j].gameover){
                         for(int k = 0;k<height;k++){
                             for(int h = 0;h<width;h++){
-                                int a =  P[i][j].point[k][h].numbers*3 + P[i][j].point[k][h].eatnumber;
+                                int a = P[i][j].point[k][h].eatnumber - P[i][j].eatenemy - P[i][j].point[k][h].numbers*3 ;
                                 if(a > max){
                                     P[i][j].punish  = a;
                                     max = a;
@@ -345,14 +342,14 @@ void algorithm_A2(Board board, Player player, int index[]){
                 
             }
         }
-        File<<firststep<<endl;
-        for(int i = 0;i<height;i++){
+        //File<<firststep<<endl;
+        /*for(int i = 0;i<height;i++){
             for(int j = 0;j<width;j++){
                 File<<P[i][j].score;
             }
            File<<endl;
-        }
-        int max = P[I][J].score - P[I][J].punish;
+        }*/
+        int max = P[I][J].score + P[I][J].punish;
         index[0] = I;
         index[1] = J;
         for(int i = 0;i<height;i++){
@@ -362,7 +359,7 @@ void algorithm_A2(Board board, Player player, int index[]){
                         P[i][j].score -= P[i][j].punish;
                         if(P[i][j].score > max){
                             index[0] = i;
-                            index[1] = j; 
+                            index[1] = j;
                             max = P[i][j].score;
                         }
                     }
@@ -370,7 +367,7 @@ void algorithm_A2(Board board, Player player, int index[]){
             }
         }
         
-        for(int i = 0;i<height;i++){
+        /*for(int i = 0;i<height;i++){
             for(int j = 0;j<width;j++){
                 File<<P[i][j].punish;
             }
@@ -382,15 +379,16 @@ void algorithm_A2(Board board, Player player, int index[]){
                 File<<P[i][j].score<<setw(8);
             }
            File<<endl;
-        }
+        }*/
         
-        cout<< index[0] << " "<<index[1];
+        //cout<< index[0] << " "<<index[1];
         //getch();
         return;
     }
-    ////////////defend//////////
+//////////////////////////////defend/////////////////////////////////
     
     else {
+        File<<"method2"<<endl;
         tmp = board;
         ////////把不能清除所有被gameover的點清除
         for(int i = 0;i<height;i++){
@@ -398,6 +396,7 @@ void algorithm_A2(Board board, Player player, int index[]){
                 tmp = board;
                 if(board.get_cell_color(i,j) != enemy_color){
                     tmp.place_orb(i,j,&player);
+                    //找能夠清除所有gameover的點
                     for(int k = 0;k<height;k++){
                         for(int h = 0 ;h<width;h++){
                             if(Enemy[k][h].occupy){
@@ -425,40 +424,56 @@ void algorithm_A2(Board board, Player player, int index[]){
                     for(int k =0;k<height;k++){
                         for(int h = 0;h<width;h++){
                             tmp = tmp1;
-                            tmp.place_orb(i,j,enemy);
-                            if(tmp.get_cell_color(k,h) != tmp1.get_cell_color(k,h)){
-                                P[i][j].point[k][h].numbers ++;
-                            }
-                            if(tmp.get_cell_color(k,h) != tmp1.get_cell_color(k,h) && tmp1.get_orbs_num(k,h) == boardcost[k][h]-1){
-                                P[i][j].point[k][h].eatnumber ++;
-                            }
-                            if(tmp.get_cell_color(i,j) != tmp1.get_cell_color(i,j)){
-                                P[i][j].point[k][h].BeOccupy = true;
-                            }
-                            for(int a =0;k<height;k++){
-                                for(int b = 0;h<width;h++){
-                                    tmp.place_orb(i,j,enemy);
-                                    if(tmp.win_the_game(*enemy)){
-                                        P[i][j].point[k][h].gameover ++; //
+                            if(tmp.get_cell_color(k,h) != color){
+                                tmp.place_orb(k,j,enemy);
+                                for(int a = 0;a<height;a++){
+                                    for(int b = 0;b<width;b++){
+                                        if(tmp.get_cell_color(a,b) != tmp1.get_cell_color(a,b) || tmp.get_orbs_num(a,b) != tmp1.get_orbs_num(a,b)){
+                                            P[i][j].point[k][h].numbers ++;
+                                        }
+                                        if(tmp.get_cell_color(a,b) != tmp1.get_cell_color(a,b) && tmp1.get_orbs_num(a,b) == tmp.get_capacity(a,b)-1){
+                                            P[i][j].point[k][h].eatnumber ++;
+                                        }
+                                        if(a == i && b == j && tmp.get_cell_color(a,b) != tmp1.get_cell_color(a,b)){
+                                            P[i][j].point[k][h].BeOccupy = true;
+                                            P[i][j].waste == true;
+                                        }
+                                    }
+                                }
+
+                                //下下次的ｇａｍｅｏｖｅｒ
+                                for(int a =0;a<height;a++){
+                                    for(int b = 0;b<width;b++){
+                                        tmp.place_orb(i,j,enemy);
+                                        if(tmp.win_the_game(*enemy)){
+                                            P[i][j].point[k][h].gameover ++; 
+                                        }
                                     }
                                 }
                             }
                         }
                     }
-                    int max = 0;;
+
+
+                    int first = 0;
+                    int max;
                     for(int k = 0;k<height;k++){
                         for(int h = 0;h<width;h++){
-                            max = P[i][j].point[k][h].eatnumber *10 + P[i][j].point[k][h].numbers + P[i][j].point[k][h].gameover*10;
-                            if(P[i][j].punish<max){
-                                P[i][j].punish = max;
+                            if(!P[i][j].point[k][h].BeOccupy){
+                                int a = P[i][j].point[k][h].numbers - P[i][j].point[k][h].eatnumber *3 - P[i][j].point[k][h].gameover*10;
+                                if(first == 0) max = a;
+                                if(a<max){
+                                    P[i][j].punish = a;
+                                    max = a;
+                                }
+                                first ++;
                             }
                         }
-
                     }
                 }
                 for(int k =0;k<height;k++){
                     for(int h = 0;h<width;h++){
-                        P[i][j].score -= P[i][j].punish;
+                        P[i][j].score += P[i][j].punish;
                     }
                 }
             }
@@ -468,15 +483,15 @@ void algorithm_A2(Board board, Player player, int index[]){
         index[1] = J;
         for(int i= 0 ;i<height;i++){
             for(int j = 0;j<width;j++){
-                if(P[i][j].score> max){
-                    max = P[i][j].score;
-                    index[0] = i;
-                    index[1] = j;
+                if(!P[i][j].waste){
+                    if(P[i][j].score> max){
+                        max = P[i][j].score;
+                        index[0] = i;
+                        index[1] = j;
+                    }
                 }
             }
         }
-        //cout<<index[0]<<" "<<index[1]<<"G";
-        
         return;
     }
 }    
